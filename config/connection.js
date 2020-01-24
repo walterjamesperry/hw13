aac53c99710afeb425700408dbe529223be79d6c
Walter Perry <walterjamesperry@gmail.com>
2020-01-23 21:03:05 -0600

    init commit

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+var mysql = require("mysql");
+
+var connection;
+
+if (process.env.JAWS_URL) {
+  connection = mysql.createConnection(process.env.JAWS_URL);
+
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+
+}
+
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
